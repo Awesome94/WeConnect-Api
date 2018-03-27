2252c8b9f659a448add4a8ebe8249f15a177aceb
Leni Kadali <lenikmutungi@gmail.com>
2018-03-27 22:02:37 +0300

    Add test for the `update_business` API endpoint
    Added the test for the `update_business` API endpoint.
    Test does not pass.

--- test_views.py
+++ test_views.py
@@ -56,6 +56,29 @@ class WeConnectViews(unittest.TestCase):
         self.assertIn(b'user_id', response.data)
         self.assertEqual(response.status_code, 201)
 
+    def test_update_business(self):
+        self.weconnect_test.post('/api/v1/auth/register', content_type='application/json',
+                                 data=json.dumps(dict(first_name='Harry', last_name='Potter',
+                                                      email='harry@aol.com', password='dumbledore')))
+        login = self.weconnect_test.post('/api/v1/auth/login', content_type='application/json',
+                                         data=json.dumps(dict(email='harry@aol.com', password='dumbledore')))
+        resp = json.loads(login.data.decode())
+        access_token = resp['access_token']
+        business = self.weconnect_test.post('/api/v1/businesses', content_type='application/json',
+                                        data=json.dumps(dict(name='Mortal Kombat', location='Earth', category='something',
+                                                        description='something', review=[])),
+                                                        headers={'Authorization': 'Bearer %s' % access_token})
+        biz_id = json.loads(business.get_data())['business']['business_id']
+        response = self.weconnect_test.put('/api/v1/businesses/'+str(biz_id), content_type='application/json',
+                                           data=json.dumps(dict(name='Mortal Kombat1', location='something_new',
+                                                                category='something_new', description='something_new')),
+                                           headers={'Authorization': 'Bearer %s' % access_token})
+
+        self.assertIn(b'Mortal Kombat1', response.data)
+        self.assertIn(b'something_new', response.data)
+        self.assertIn(b'user_id', response.data)
+        self.assertTrue(response.status_code, 201)
+
     # def test_get_business(self):
     #     self.weconnect_test.post('/api/v1/auth/register', content_type='application/json',
     #                              data=json.dumps(dict(first_name='Harry', last_name='Potter',
