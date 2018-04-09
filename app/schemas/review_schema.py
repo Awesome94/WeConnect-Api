f148afa6595f5998d4a4d687bb74e30bd916fb60
Leni Kadali <lenikmutungi@gmail.com>
2018-04-09 22:36:13 +0300

    Add the review_schema module to schemas folder
    Added the review_schema module to the schemas folder. It is
    meant to be used for validation of JSON data entered into the API.

---
+++ app/schemas/review_schema.py
@@ -0,0 +1,24 @@
+review_schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "review-schema",
+    "description": "Schema of post data for creating a new review \
+                    in the WeConnect app.",
+    "type": "object",
+    "properties": {
+        "business_id": {
+            "type": "string",
+            "description": "The unique id of the business",
+            "maxLength": 255
+        },
+        "review_id": {
+            "type": "string",
+            "description": "The description of the business",
+            "maxLength": 255
+        },
+        "review": {
+            "type": "string"
+        }
+    },
+    "additionalProperties": False,
+    "required": ["review"]
+}
