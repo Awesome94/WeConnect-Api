3fd6cc7939adb36c09d821a83778cc004d429802
Leni Kadali <lenikmutungi@gmail.com>
2018-04-10 17:06:18 +0300

    Modify users view module to use schemas
    Modified users view module to use schemas.

--- app/v1/users/views.py
+++ app/v1/users/views.py
@@ -6,7 +6,12 @@ from flask_jwt_extended import (JWTManager, create_access_token,
                                 jwt_required)
 
 from app import app
+from app.schemas.user_schema import user_schema
+from app.schemas.login_schema import login_schema
+from app.schemas.reset_schema import reset_schema
 from app.v1.models import WeConnect
+from app.v1.validator import validate_schema
+from validate_email import validate_email
 
 app.config['JWT_SECRET_KEY'] = os.urandom(23)
 app.config['JWT_BLACKLIST_ENABLED'] = True
@@ -24,15 +29,14 @@ def check_if_token_in_blacklist(decrypted_token):
 
 
 @app.route('/api/v1/auth/register', methods=['POST'])
+@validate_schema(user_schema)
 def register_user():
     data = request.get_json()
     email = data['email']
     first_name = data['first_name']
     last_name = data['last_name']
     password = data['password']
-    if not data or not email:
-        abort(400)
-    if email is None:
+    if validate_email(email) == False:
         abort(400)
     new_user = weconnect.register_user(random.randint(1, 500),
                                        first_name, last_name, email, password)
@@ -41,16 +45,16 @@ def register_user():
 
 
 @app.route('/api/v1/auth/login', methods=['POST'])
+@validate_schema(login_schema)
 def login_user():
     data = request.get_json()
     email = data['email']
     password = data['password']
-    if not data or not email:
-        abort(400)
     user = weconnect.login_user(email, password)
     if user:
         access_token = create_access_token(identity=user)
-        return jsonify(access_token=access_token), 200
+        if access_token not in blacklist:
+            return jsonify(access_token=access_token), 200
     else:
         abort(404)
 
@@ -65,20 +69,17 @@ def logout():
 
 @app.route('/api/v1/auth/reset-password', methods=['POST'])
 @jwt_required
+@validate_schema(reset_schema)
 def reset_password():
     data = request.get_json()
     email = data['email']
     password = data['password']
     new_password = data['new_password']
-    if not data or not email or not password or not new_password:
-        return jsonify(
-            {'message': 'Supply your password and/or a new password'}), 401
     user = weconnect.reset_password(email, password, new_password)
     if user:
         return jsonify({'message': 'Successfully updated password'}), 200
-    else:
-        return jsonify(
-            {'message': 'Supply your password and/or a new password'}), 401
+    return jsonify(
+        {'message': 'Supply your password and/or a new password'}), 401
 
 
 @app.errorhandler(404)
