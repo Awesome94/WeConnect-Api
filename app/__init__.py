e180b44b043d3e571b2c1b0e5f8b898b6d4cb5aa
Leni Kadali <lenikmutungi@gmail.com>
2018-04-16 16:00:34 +0300

    Add application factory and import settings from config module
    Added application factory (create_app) and imported configuration
    settings from the config module. Configuration is loaded within
    create_app.
    
    The factory function returns the created application instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
 
-app = Flask(__name__)
-
 from app.v1.businesses import views
 from app.v1.users import views
-#app.config.from_object('config')
+from config import config
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    return app
