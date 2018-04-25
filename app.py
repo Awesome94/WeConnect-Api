387d9b206a8812a4b6acb480f31b00b9482b73de
Leni Kadali <lenikmutungi@gmail.com>
2018-04-26 00:02:22 +0300

    Revert changes in app module
    Reverted changes in app module. Will restore once able
    customize the create_app function.

--- app.py
+++ app.py
@@ -1,44 +1,53 @@
-import click, os, sys, unittest
-from app import create_app
-from dotenv import load_dotenv
+import click, unittest
+from app import app
+# from dotenv import load_dotenv
 from app.v1.models import WeConnect
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# if os.path.exists(dotenv_path):
+#     load_dotenv(dotenv_path)
 
-COV = None
-if os.environ.get('FLASK_COVERAGE'):
-    import coverage
-    COV = coverage.coverage(branch=True, include='app/*')
-    COV.start()
-
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(app=app, WeConnect=WeConnect)
-
+app.run(debug=True)
 
 @app.cli.command()
-@click.option('--coverage/--no-coverage', default=False,
-help='Run tests under code coverage.')
-def test(coverage):
-    """Runs the unit tests."""
-    if coverage and not os.environ.get('FLASK_COVERAGE'):
-        import subprocess
-        os.environ['FLASK_COVERAGE'] = '1'
-        sys.exit(subprocess.call(sys.argv))
-
+def test():
+    """Runs the unit tests without test coverage."""
     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if COV:
-        COV.stop()
-        COV.save()
-        print('Coverage Summary:')
-        COV.report()
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, 'tmp/coverage')
-        COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covdir)
-        COV.erase()
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+# COV = None
+# if os.environ.get('FLASK_COVERAGE'):
+#     import coverage
+#     COV = coverage.coverage(branch=True, include='app/*')
+#     COV.start()
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return dict(app=app, WeConnect=WeConnect)
+
+
+# @app.cli.command()
+# @click.option('--coverage/--no-coverage', default=False,
+# help='Run tests under code coverage.')
+# def test(coverage):
+#     """Runs the unit tests."""
+#     if coverage and not os.environ.get('FLASK_COVERAGE'):
+#         import subprocess
+#         os.environ['FLASK_COVERAGE'] = '1'
+#         sys.exit(subprocess.call(sys.argv))
+
+#     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+#     result = unittest.TextTestRunner(verbosity=2).run(tests)
+#     if COV:
+#         COV.stop()
+#         COV.save()
+#         print('Coverage Summary:')
+#         COV.report()
+#         basedir = os.path.abspath(os.path.dirname(__file__))
+#         covdir = os.path.join(basedir, 'tmp/coverage')
+#         COV.html_report(directory=covdir)
+#         print('HTML version: file://%s/index.html' % covdir)
+#         COV.erase()
