69f3c83d41d0f650db438a66bc0e6682af7c3b0c
Leni Kadali <lenikmutungi@gmail.com>
2018-05-02 13:28:31 +0300

    Re-write the return statement in the app module
    Re-wrote the return statement in the app module

--- app.py
+++ app.py
@@ -14,10 +14,8 @@ def test():
     """Runs the unit tests without test coverage."""
     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
-
+    return 0 if result.wasSuccessful() else 1
+    
 # COV = None
 # if os.environ.get('FLASK_COVERAGE'):
 #     import coverage
