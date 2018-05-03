fcfb40d71474f659fa0a592488fb5baa2342d2ac
Leni Kadali <lenikmutungi@gmail.com>
2018-05-03 19:41:39 +0300

    Move tests to main tests folder
    Moved tests to main tests folder, deleting the tests_v1 folder
    to be in line with Flask best practices.

---
+++ tests/test_reviews.py
@@ -0,0 +1,114 @@
+import flask
+import json
+import flask_jwt_extended
+import unittest
+
+from app import app
+from app.v1 import models
+from app.v1.views import businesses, reviews, users
+
+class WeConnectViews(unittest.TestCase):
+    """Tests the enpoints contains in views.py"""
+
+    def setUp(self):
+        self.weconnect_test = app.test_client(self)
+        models.WeConnect.user_db = []
+        models.WeConnect.business_db = []
+
+    def tearDown(self):
+        models.WeConnect.user_db = []
+        models.WeConnect.business_db = []
+
+    def test_add_review(self):
+        self.weconnect_test.post(
+            '/api/v1/auth/register',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    first_name='Harry',
+                    last_name='Potter',
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        login = self.weconnect_test.post(
+            '/api/v1/auth/login',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        resp = json.loads(login.data.decode())
+        access_token = resp['access_token']
+        business = self.weconnect_test.post(
+            '/api/v1/businesses',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    name='Mortal Kombat',
+                    location='Earth',
+                    category='something',
+                    description='something',
+                    review=[])),
+            headers={
+                'Authorization': 'Bearer %s' % access_token})
+        biz_id = json.loads(business.data.decode())['business']['business_id']
+        review = self.weconnect_test.post(
+            '/api/v1/businesses/' + str(biz_id) + '/reviews',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    review="Beautiful")),
+            headers={
+                'Authorization': 'Bearer %s' % access_token})
+        self.assertIn(b'review_id', review.data)
+        self.assertTrue(review.status_code, 201)
+
+    def test_get_reviews(self):
+        self.weconnect_test.post(
+            '/api/v1/auth/register',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    first_name='Harry',
+                    last_name='Potter',
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        login = self.weconnect_test.post(
+            '/api/v1/auth/login',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        resp = json.loads(login.data.decode())
+        access_token = resp['access_token']
+        business = self.weconnect_test.post(
+            '/api/v1/businesses',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    name='Mortal Kombat',
+                    location='Earth',
+                    category='something',
+                    description='something',
+                    review=[])),
+            headers={
+                'Authorization': 'Bearer %s' % access_token})
+        biz_id = json.loads(business.data.decode())['business']['business_id']
+        self.weconnect_test.post(
+            '/api/v1/businesses/' + str(biz_id) + '/reviews',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    review="Beautiful")),
+            headers={
+                'Authorization': 'Bearer %s' % access_token})
+        get_review = self.weconnect_test.get(
+            '/api/v1/businesses/' + str(biz_id) + '/reviews',
+            content_type='application/json',
+            headers={
+                'Authorization': 'Bearer %s' % access_token})
+        self.assertTrue(get_review.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_users.py
@@ -0,0 +1,82 @@
+import flask
+import json
+import flask_jwt_extended
+import unittest
+
+from app import app
+from app.v1 import models
+from app.v1.views import users
+
+class WeConnectViews(unittest.TestCase):
+    """Tests the enpoints contains in views.py"""
+
+    def setUp(self):
+        self.weconnect_test = app.test_client(self)
+        models.WeConnect.user_db = []
+        models.WeConnect.business_db = []
+
+    def tearDown(self):
+        models.WeConnect.user_db = []
+        models.WeConnect.business_db = []
+
+    def test_register_user(self):
+        response = self.weconnect_test.post(
+            '/api/v1/auth/register',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    first_name='Harry',
+                    last_name='Potter',
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_user(self):
+        self.weconnect_test.post(
+            '/api/v1/auth/register',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    first_name='Harry',
+                    last_name='Potter',
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        response = self.weconnect_test.post(
+            '/api/v1/auth/login',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        self.assertEqual(response.status_code, 200)
+
+    def test_reset_password(self):
+        self.weconnect_test.post(
+            '/api/v1/auth/register',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    first_name='Harry',
+                    last_name='Potter',
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        login = self.weconnect_test.post(
+            '/api/v1/auth/login',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    email='harry@aol.com',
+                    password='dumbledore')))
+        resp = json.loads(login.data.decode())
+        access_token = resp['access_token']
+        response = self.weconnect_test.post(
+            '/api/v1/auth/reset-password',
+            content_type='application/json',
+            data=json.dumps(
+                dict(
+                    email='harry@aol.com',
+                    password='dumbledore',
+                    new_password='severus snape')),
+            headers={
+                'Authorization': 'Bearer %s' % access_token})
+        self.assertEqual(response.status_code, 200)
